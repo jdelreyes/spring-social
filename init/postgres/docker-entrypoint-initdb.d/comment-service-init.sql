527c06750c4fc0e63df178f72a187a2ebf992bed
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2023-11-07 01:22:19 -0500

    fixed conflict

--- init/postgres/docker-entrypoint-initdb.d/comment-service-init.sql
+++ init/postgres/docker-entrypoint-initdb.d/comment-service-init.sql
@@ -1,7 +1,4 @@
 CREATE DATABASE "comment-service";
 
-<<<<<<< HEAD
-GRANT ALL PRIVILEGES ON DATABASE "comment-service" TO "admin"
-=======
 GRANT ALL PRIVILEGES ON DATABASE "comment-service" TO "rootadmin"
->>>>>>> 36f7e28 (refactored init files for potgresql)
+
--- init/postgres/docker-entrypoint-initdb.d/user-service-init.sql
+++ init/postgres/docker-entrypoint-initdb.d/user-service-init.sql
@@ -1,7 +1,3 @@
 CREATE DATABASE "user-service";
 
-<<<<<<< HEAD
-GRANT ALL PRIVILEGES ON DATABASE "user-service" TO "admin"
-=======
 GRANT ALL PRIVILEGES ON DATABASE "user-service" TO "rootadmin"
->>>>>>> 36f7e28 (refactored init files for potgresql)
