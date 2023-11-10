0382e11db061b21135d6cf0bd90de6dec81229c9
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2023-11-09 20:25:10 -0500

    initialized discovery service by implementing eureka server

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "spring-social"
 
-include("user-service", "post-service", "friendship-service", "comment-service")
+include("user-service", "post-service", "friendship-service", "comment-service", "discovery-service")
