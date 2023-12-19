3e5cc1b82ec0a0e0fc0b11ef7925cd2bb63ac716
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2023-12-18 23:46:04 -0500

    initialized dockerfile and added notif-service to docker-compose.yml

--- notification-service/build.gradle.kts
+++ notification-service/build.gradle.kts
@@ -16,14 +16,28 @@ repositories {
 }
 
 dependencies {
+//    spring
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
+//    test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+//    lombok
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    implementation("org.testcontainers:testcontainers-bom:1.18.1")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
+//    eureka
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client:4.0.3")
+//	micrometer
+    implementation("io.micrometer:micrometer-observation:1.11.3")
+    implementation("io.micrometer:micrometer-tracing-bridge-brave:1.1.4")
+//	zipkin
+    implementation("io.zipkin.reporter2:zipkin-reporter-brave:2.16.4")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-resilience4j:3.0.3")
+//    kafka
+    implementation("org.springframework.kafka:spring-kafka:3.1.0")
+    testImplementation("org.springframework.kafka:spring-kafka-test:3.1.0")
 }
 
 tasks.withType<Test> {
